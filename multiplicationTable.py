8bab3820c6cc6cd5d8e0ffb5c44a677a390106f9
Shahlo <33292441+sseidmed@users.noreply.github.com>
2019-10-01 18:41:16 -0400

    Update multiplicationTable.py
    converted 'userName' variable to an integer

--- multiplicationTable.py
+++ multiplicationTable.py
@@ -12,20 +12,20 @@ fontObj = Font(bold=True)
 
 def multiplication(num, filename):
 	#populate the A column
-	for i in range(1, int(num)+1): 
+	for i in range(1, num+1): 
 		sheet['A' + str(i+1)] = i
 		sheet['A' + str(i+1)].font = fontObj
 
 	#populate the first row	
-	for r in range(1,int(num)+1): 
+	for r in range(1,num+1): 
 		col = get_column_letter(r+1)
 		columns = col+str(1)
 		sheet[columns].value = r
 		sheet[columns].font = fontObj
 
 	#populate the cells with the products
-	for k in range(1, int(num)+1):
-		for j in range(1, int(num)+1):
+	for k in range(1, num+1):
+		for j in range(1, num+1):
 			sheet.cell(row=k+1, column=j+1).value = k*j
 	print('Multiplication Table is ready to be viewed')
 
@@ -39,6 +39,6 @@ userFileName = sys.argv[2]
 if len(sys.argv) < 2:
 	print('Please enter a number, followed by a word that will be used as a filename')	
 else:
-	multiplication(userNumber, userFileName) 
+	multiplication(int(userNumber), userFileName) 
 
 
